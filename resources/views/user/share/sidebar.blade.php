f681bce9e96980d3d76951656fbed3b21eb6fcce
mahadishanto082 <mahadishanto26@gmail.com>
2025-12-07 19:57:06 +0600

    trying to fix the sidebar as functional

--- resources/views/user/share/sidebar.blade.php
+++ resources/views/user/share/sidebar.blade.php
@@ -1,69 +1,34 @@
 <div>
-    <div class="content-overlay">
-        <div class="content-edit-sidebar">
-            <div class="sidebar-header">
-                <div></div>
-                <div class="close-btn-second">
-                    <i class="fa-solid fa-xmark"></i>
-                </div>
-            </div>
-            <div class="content">
-                <span>Click on the Edit Content button to edit/add the content.</span>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div>
     <div class="sidebar-overlay"></div>
+
     <div class="sidebar">
         <div class="sidebar-header">
-        <a href="{{ route('home') }}">
-                        <img src="{{ asset('storage/' . $headerContent->logo) }}" alt="Logo" height="100">
-                    </a>
-              
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('storage/' . $headerContent->logo) }}" alt="Logo" height="100">
+            </a>
+
             <button class="close-btn"><span>X</span></button>
         </div>
+
         <ul class="menu">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About Us</a></li>
-            <li class="sidebar-dropdown">
-                <div class="dropdown-header">
-                    <a href="#">Services</a>
-                    <button class="sidebar-dropdown-btn"><i class="fa-solid fa-angle-down"></i></button>
-                </div>
-                <ul class="sidebar-dropdown-menu">
-                    <li><a href="service.html">Service</a></li>
-                    <li><a href="single_services.html">Service Details</a></li>
-                </ul>
-            </li>              
-            <li class="sidebar-dropdown">
-                <div class="dropdown-header">
-                    <a href="#">Pages</a>
-                    <button class="sidebar-dropdown-btn"><i class="fa-solid fa-angle-down"></i></button>
-                </div>
-                <ul class="sidebar-dropdown-menu">
-                    <li><a href="case_studies.html">Case Studies</a></li>
-                    <li><a href="team.html">Our Team</a></li>
-                    <li><a href="partnership.html">Partnership</a></li>
-                    <li><a href="pricing.html">Princing Plan</a></li>
-                    <li><a href="testimonial.html">Testimonial</a></li>
-                    <li><a href="faq.html">FAQs</a></li>
-                    <li><a href="404_page.html">Error 404</a></li>
-                </ul>
-            </li>  
-            <li class="sidebar-dropdown">
-                <div class="dropdown-header">
-                    <a href="#">Archive</a>
-                    <button class="sidebar-dropdown-btn"><i class="fa-solid fa-angle-down"></i></button>
-                </div>
-                <ul class="sidebar-dropdown-menu">
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="single_post.html">Single Post</a></li>
-                </ul>
-            </li>  
-            <li class="below-dropdown"><a href="contact.html">Contact Us</a></li>
+
+            {{-- 🔥 DYNAMIC BUTTONS (Same as Navbar) --}}
+            @php
+                $buttons = is_array($headerContent->buttons)
+                    ? $headerContent->buttons
+                    : (json_decode($headerContent->buttons, true) ?? []);
+            @endphp
+
+            @if(!empty($buttons))
+                @foreach($buttons as $btn)
+                    <li>
+                        <a href="{{ $btn['link'] ?? '#' }}" class="sidebar-link">
+                            {{ $btn['name'] ?? '' }}
+                        </a>
+                    </li>
+                @endforeach
+            @endif
+
         </ul>
     </div>
 </div>
-
