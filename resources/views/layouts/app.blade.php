c6f001a467d12e977b447510ff7a5c02b068a8b8
mahadishanto082 <mahadishanto26@gmail.com>
2025-12-07 20:10:36 +0600

    working on theme switcher

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,35 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- Styles & Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <style>
+        /* Dark Theme Styles */
+        .dark-theme {
+            background-color: #111 !important;
+            color: #fff !important;
+        }
+
+        .dark-theme a {
+            color: #fff !important;
+        }
+
+        .dark-theme .navbar {
+            background-color: #222 !important;
+        }
+
+        .themeswitch {
+            background: none;
+            border: none;
+            cursor: pointer;
+            font-size: 18px;
+            color: inherit;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
@@ -23,18 +50,25 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Stradigtech') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
+                    <ul class="navbar-nav me-auto"></ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav ms-auto align-items-center">
+                        <!-- Theme Switch Button -->
+                        <li class="nav-item">
+                            <button id="themeSwitch" class="themeswitch" aria-label="Toggle Theme">
+                                <i id="themeIcon" class="fas fa-moon"></i>
+                            </button>
+                        </li>
+
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -50,14 +84,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" 
+                                   data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -76,5 +110,35 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- Global Theme Switcher Script -->
+    <script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const themeSwitch = document.getElementById("themeSwitch");
+        const themeIcon = document.getElementById("themeIcon");
+
+        if (!themeSwitch || !themeIcon) return;
+
+        // Load saved theme
+        let savedTheme = localStorage.getItem("theme");
+        if (savedTheme === "dark") {
+            document.body.classList.add("dark-theme");
+            themeIcon.classList.replace("fa-moon", "fa-sun");
+        }
+
+        // Toggle theme
+        themeSwitch.addEventListener("click", () => {
+            document.body.classList.toggle("dark-theme");
+
+            if (document.body.classList.contains("dark-theme")) {
+                localStorage.setItem("theme", "dark");
+                themeIcon.classList.replace("fa-moon", "fa-sun");
+            } else {
+                localStorage.setItem("theme", "light");
+                themeIcon.classList.replace("fa-sun", "fa-moon");
+            }
+        });
+    });
+    </script>
 </body>
 </html>
