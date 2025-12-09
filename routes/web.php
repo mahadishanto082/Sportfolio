4298e641ce9dd6ca4e6f49ce86b5566865e14f6c
mahadishanto082 <mahadishanto26@gmail.com>
2025-12-09 22:32:16 +0600

    controller fixed

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Admin\Auth\LoginController as AdminLoginController;
-use App\Http\Controllers\Website\ContactMessageController;
+use App\Http\Controllers\ContactMessageController;
+
+
 
 Route::get('/', [UserController::class, 'home'])->name('home');
+
 
 Route::get('admin/login', [AdminLoginController::class, 'showLoginForm'])->name('admin.login');
 
@@ -15,4 +19,4 @@ Route::get('/pages/services', [UserController::class, 'services'])->name('pages.
 Route::get('/pages/projects', [UserController::class, 'projects'])->name('pages.projects');
 Route::get('/pages/contact', [UserController::class, 'contact'])->name('pages.contact');
 // Contact Message Routes
-Route::post('/contact/messages', [ContactMessageController::class, 'store'])->name('contact.store');
+Route::post('/contact-messages', [ContactMessageController::class, 'store'])->name('contact.store');
