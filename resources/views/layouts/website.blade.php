1ed5c4fda5425a1f4783355369f2d0120d6d8d16
mahadishanto082 <mahadishanto26@gmail.com>
2025-12-09 21:24:33 +0600

    test the css

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -9,7 +9,17 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
+    <style>
+        /* Test Dark Mode Styles */
+        body.dark-mode {
+            background-color: #1a1a1a !important;
+            color: #ffffff !important;
+        }
+        
+        body.dark-mode * {
+            border-color: #444 !important;
+        }
+    </style>
 </head>
 <body>
     <!-- Header / Navbar -->
