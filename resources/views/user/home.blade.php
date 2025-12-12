dd158dac46b258950e67ab82bda591d67f42642f
mahadishanto082 <mahadishanto26@gmail.com>
2025-12-12 18:23:04 +0600

    removed js

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -373,12 +373,5 @@
     </div>
 </main>
 
-<script src="js/vendor/jquery.min.js"></script>
-<script src="js/vendor/bootstrap.bundle.min.js"></script>
-<script src="js/vendor/swiper-bundle.min.js"></script>
-<script src="js/vendor/isotope.pkgd.min.js"></script>
-<script src="js/script.js"></script>
-<script src="js/swiper-script.js"></script>
-<script src="js/submit-form.js"></script>
-<script src="js/video_embedded.js"></script>
+
 @endsection
